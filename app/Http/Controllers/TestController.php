a9ff1fca22ddd00231e5bab9cae4be2530580244
Ghulam Ahmed <ghlama29@gmail.com>
2025-04-02 18:45:30 +0500

    test controller added.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,13 +16,36 @@ class TestController extends Controller
      */
     public function index(Request $request){
 
+        $customersList = \Acelle\Model\MailList::where(['name'=>'Customers Emails','customer_id'=>1])->first();
+
         $users = \Acelle\Model\User::get();
 
         foreach ($users as $user) {
-                
-            $user->url = $user->customerLandingPageUrl();
 
-            $user->save();
+            if(!$user->can("admin_access", $user)){
+                if(!empty($customersList)){
+                    
+                    $checkSubscriber = \Acelle\Model\Subscriber::where(['mail_list_id'=>$customersList->id,"email"=>$user->email])->first();
+                    if(empty($checkSubscriber)){
+                        $subscriber = new \Acelle\Model\Subscriber();
+
+                        $subscriber->mail_list_id = $customersList->id;
+
+                        $subscriber->email = $user->email;
+
+                        $subscriber->custom_100 = $user->email;
+                        $subscriber->custom_101 = $user->first_name;
+                        $subscriber->custom_102 = $user->last_name;
+
+                        $subscriber->status = 'subscribed';
+
+                        $subscriber->save();
+                    }
+                }
+            }
+
+            // $user->url = $user->customerLandingPageUrl();
+            // $user->save();
 
         }
 
