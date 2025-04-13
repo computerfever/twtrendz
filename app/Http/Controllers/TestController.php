2fda2b1da57f6e88f6461a19b5b260bcee43ab98
Ghulam Ahmed <ghlama29@gmail.com>
2025-04-13 12:20:41 +0500

    test controller updated.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,38 +16,55 @@ class TestController extends Controller
      */
     public function index(Request $request){
 
-        $customersList = \Acelle\Model\MailList::where(['name'=>'Customers Emails','customer_id'=>1])->first();
+        $logs = \Acelle\Model\BounceLog::where('raw', 'like', '"{%')->get();
 
-        $users = \Acelle\Model\User::get();
+        foreach ($logs as $log) {
 
-        foreach ($users as $user) {
+            // $log->raw = str_replace(["\\"], "", $log->raw);
+            // $log->raw = str_replace(['"{','}"'], ['{','}'], $log->raw);
 
-            if(!$user->can("admin_access", $user)){
-                if(!empty($customersList)){
-                    
-                    $checkSubscriber = \Acelle\Model\Subscriber::where(['mail_list_id'=>$customersList->id,"email"=>$user->email])->first();
-                    if(empty($checkSubscriber)){
-                        $subscriber = new \Acelle\Model\Subscriber();
+            $raw = json_decode($log->raw);
 
-                        $subscriber->mail_list_id = $customersList->id;
+            $log->raw = $raw;
 
-                        $subscriber->email = $user->email;
+            $log->save();
 
-                        $subscriber->custom_100 = $user->email;
-                        $subscriber->custom_101 = $user->first_name;
-                        $subscriber->custom_102 = $user->last_name;
-
-                        $subscriber->status = 'subscribed';
-
-                        $subscriber->save();
-                    }
-                }
-            }
-
-            // $user->url = $user->customerLandingPageUrl();
-            // $user->save();
+            // print_r($raw);
 
         }
+
+        // $customersList = \Acelle\Model\MailList::where(['name'=>'Customers Emails','customer_id'=>1])->first();
+
+        // $users = \Acelle\Model\User::get();
+
+        // foreach ($users as $user) {
+
+        //     if(!$user->can("admin_access", $user)){
+        //         if(!empty($customersList)){
+                    
+        //             $checkSubscriber = \Acelle\Model\Subscriber::where(['mail_list_id'=>$customersList->id,"email"=>$user->email])->first();
+        //             if(empty($checkSubscriber)){
+        //                 $subscriber = new \Acelle\Model\Subscriber();
+
+        //                 $subscriber->mail_list_id = $customersList->id;
+
+        //                 $subscriber->email = $user->email;
+
+        //                 $subscriber->custom_100 = $user->email;
+        //                 $subscriber->custom_101 = $user->first_name;
+        //                 $subscriber->custom_102 = $user->last_name;
+
+        //                 $subscriber->status = 'subscribed';
+
+        //                 $subscriber->save();
+        //             }
+        //         }
+        //     }
+
+        //     // $user->url = $user->customerLandingPageUrl();
+        //     // $user->save();
+
+        // }
 
     }   
 }
