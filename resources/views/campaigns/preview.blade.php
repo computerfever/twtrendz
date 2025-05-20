53c4749f2df4d3860c6221f125b8da50487181e2
Ghulam Ahmed <ghlama29@gmail.com>
2025-05-20 10:39:16 +0500

    share issue fixed.

--- resources/views/campaigns/preview.blade.php
+++ resources/views/campaigns/preview.blade.php
@@ -185,7 +185,7 @@
 					<li class="button__share button__share--stumbleupon"><a href="{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => Auth::user()->customer->uid ])}}" target="_blank">Public Url</a></li>
 
 					<li class="button__share button__share--facebook">
-						<a href="javascript:void(window.open('https://www.facebook.com/sharer.php?u=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => $campaign->customer->uid ])}}') + '?t=' + encodeURIComponent(document.title),'_blank'))">
+						<a href="javascript:void(window.open('https://www.facebook.com/sharer.php?u=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => Auth::user()->customer->uid ])}}') + '?t=' + encodeURIComponent(document.title),'_blank'))">
 							<span class="d-flex align-items-center">
 								<span class="me-2">
 									<svg style="height:20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 404.1 800"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M12.9,426.1h87.7v361A12.9,12.9,0,0,0,113.5,800H262.2a12.9,12.9,0,0,0,12.9-12.9V427.8H375.9a12.8,12.8,0,0,0,12.8-11.4l15.3-133a12.7,12.7,0,0,0-3.2-10,12.5,12.5,0,0,0-9.6-4.3H275.1V185.7c0-25.1,13.5-37.8,40.2-37.8h75.9A12.9,12.9,0,0,0,404.1,135V13A12.9,12.9,0,0,0,391.2.1H281.8c-18.2,0-81.3,3.6-131.1,49.4C95.5,100.2,103.1,161.1,105,171.6v97.5H12.9A12.9,12.9,0,0,0,0,282V413.2A12.9,12.9,0,0,0,12.9,426.1Z" style="fill:#fff"/></g></g></svg>
@@ -195,7 +195,7 @@
 						</a>
 					</li>
 					<li class="button__share button__share--twitter">
-						<a href="javascript:void(window.open('https://twitter.com/share?url=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => $campaign->customer->uid ])}}') + '&amp;text=' + encodeURIComponent(document.title),'_blank'))">
+						<a href="javascript:void(window.open('https://twitter.com/share?url=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => Auth::user()->customer->uid ])}}') + '&amp;text=' + encodeURIComponent(document.title),'_blank'))">
 							<span class="d-flex align-items-center">
 								<span class="me-2">
 									<svg style="height:20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 246.2 200"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M221,49.8c.1,2.2.1,4.3.1,6.5,0,66.8-50.8,143.7-143.7,143.7h0A142.8,142.8,0,0,1,0,177.3a96.7,96.7,0,0,0,12,.8,100.9,100.9,0,0,0,62.7-21.7,50.4,50.4,0,0,1-47.1-35.1,50.3,50.3,0,0,0,22.8-.8A50.5,50.5,0,0,1,9.9,71v-.7a49.4,49.4,0,0,0,22.9,6.3A50.5,50.5,0,0,1,17.1,9.2,143.6,143.6,0,0,0,121.2,62a50.6,50.6,0,0,1,86.1-46.1A103.4,103.4,0,0,0,239.4,3.7a51.2,51.2,0,0,1-22.2,27.9,101.7,101.7,0,0,0,29-8A104.6,104.6,0,0,1,221,49.8Z" style="fill:#fff"/></g></g></svg>
@@ -207,7 +207,7 @@
 					<!-- optional Twitter username of content author (don’t include “@”)
 					optional Hashtags appended onto the tweet (comma separated. don’t include “#”) -->
 					<li class="button__share button__share--linkedin">
-						<a href="javascript:void(window.open('https://www.linkedin.com/shareArticle?url=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => $campaign->customer->uid ])}}') + '&amp;title=' + encodeURIComponent(document.title),'_blank'))">
+						<a href="javascript:void(window.open('https://www.linkedin.com/shareArticle?url=' + encodeURIComponent('{{action('Pub\CampaignController@previewContent', [ 'uid' => $campaign->uid, 'customer_uid' => Auth::user()->customer->uid ])}}') + '&amp;title=' + encodeURIComponent(document.title),'_blank'))">
 							<span class="d-flex align-items-center">
 								<span class="me-2">
 									<svg style="height:20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.4 51"><g id="Layer_2" data-name="Layer 2"><g id="svg3070"><path id="path28" d="M12.1,51V16.6H.7V51ZM6.4,11.9c4,0,6.5-2.6,6.5-6S10.4,0,6.5,0,0,2.6,0,5.9s2.5,6,6.3,6Z" style="fill:#fff;fill-rule:evenodd"/><path id="path30" d="M18.5,51H29.9V31.8a7.9,7.9,0,0,1,.4-2.8,6.3,6.3,0,0,1,5.8-4.2c4.2,0,5.8,3.2,5.8,7.8V51H53.4V31.3c0-10.6-5.7-15.5-13.2-15.5a11.3,11.3,0,0,0-10.4,5.8h.1v-5H18.5c.1,3.2,0,34.4,0,34.4Z" style="fill:#fff;fill-rule:evenodd"/></g></g></svg>
